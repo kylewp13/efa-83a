45c95907d3f81ccec03bd74cda7ef5355c456d69
Ryan Krenzke <ryan.krenzke@gmail.com>
2021-07-01 20:36:13 -0400

    pushing branch test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Weather />
     </div>
   );
 }
