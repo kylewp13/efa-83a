7bad3d35dbf7885edbe2a2fb153cdd9122ead66d
Chelsey Tschida <chelsey.tschida@outlook.com>
2021-07-03 13:21:28 -0400

    fetching and displaying tm data

--- src/components/Ticketmaster.js
+++ src/components/Ticketmaster.js
@@ -1,13 +1,36 @@
+import React, { useState, useEffect } from 'react'
+
 const Ticketmaster = ({ lat, long }) => {
-  fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${process.env.REACT_APP_TM_API_KEY}&latlong=${lat}${long}`
-  )
-    .then(res => res.json())
-    .then(json => console.log(json))
+  const [events, setEvents] = useState([])
+
+  const url = `https://app.ticketmaster.com/discovery/v2/events.json?&apikey=${process.env.REACT_APP_TM_API_KEY}&latlong=`
+  const latlon = lat + ',' + long
+
+  const fetchEvents = async () => {
+    try {
+      const res = await fetch(url + latlon)
+      const data = await res.json()
+      console.log(data)
+      setEvents(data._embedded.events)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  //   fetch(url + latlon)
+  //     .then(res => res.json())
+  //     .then(json => console.log(json))
+
+  useEffect(() => {
+    fetchEvents()
+  })
 
   return (
     <div>
       <h1>This is Ticketmaster</h1>
+      {events?.map((event, index) => (
+        <h2 key={index}>{event.name}</h2>
+      ))}
     </div>
   )
 }
